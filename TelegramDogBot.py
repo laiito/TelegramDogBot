94c5bf40058ba7d6f584a1af5697e338961b6952
laiito <maxim20demyanov@gmail.com>
2021-01-08 18:23:42 +0300

    Little fixes, removed code duplication

--- TelegramDogBot.py
+++ TelegramDogBot.py
@@ -1,23 +1,27 @@
-import telebot  # pytelegrambotapi
+import telebot
 import requests
 import random
 import time
 from bs4 import BeautifulSoup
-
-from token import TOKEN
+from mytoken import MY_TOKEN
 """
-    Create 'token.py' with your token inside, e.g.:
-        TOKEN = 'PLACE_YOUR_TOKEN_HERE'
+    Create 'mytoken.py' with your HTTP API access token, e.g.:
+        MY_TOKEN = 'PLACE_YOUR_TOKEN_HERE'
 """
-
 # TODO: add documentation wherever is possible
+
+BASE_URL = ('http://joyreactor.cc/search/+/', '?tags=гифки%2C+')
+ANIMAL_DICT = {
+    'Хочу пёсу!': 'собака',
+    'Хочу коту!': 'котэ'
+    # TODO: add parrot
+}
 
 
 def get_mp4_url(animal):
-    base_url = ('http://joyreactor.cc/search/+/', '?tags=гифки%2C+')
     page_number = random.randint(1, 100)
     post_number = random.randint(0, 9)
-    url = base_url[0] + str(page_number) + base_url[1] + animal
+    url = BASE_URL[0] + str(page_number) + BASE_URL[1] + animal
     contents = requests.get(url).text
     soup = BeautifulSoup(contents, 'lxml')
     post_list = soup.find_all('div', attrs={'class': 'postContainer'})
@@ -36,49 +40,44 @@ def get_mp4_url(animal):
 def try_send(func, chat, unswer, keyboard):
     try:
         func_dict[func](chat.id, unswer, reply_markup=keyboard)
-    except Exception:
+    except:
         print_log('Connection error. Reconnecting...', chat.first_name)
         time.sleep(5)
         try_send(func, chat, unswer, keyboard)
 
 
 def print_log(command, name):
+    # TODO: logging to file; adjust columns
     now = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime())
     print(now + '	' + command + '	from	' + name)
 
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(MY_TOKEN)
 # TODO: Use 'types.InlineKeyboardMarkup' instead
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
-keyboard1.row('Хочу пёсу!', 'Хочу коту!')
+keyboard = telebot.types.ReplyKeyboardMarkup(True)
+keyboard.row(*ANIMAL_DICT)
 
 func_dict = {
     'message': bot.send_message,
     'mp4': bot.send_animation,
-    'photo': bot.send_photo # not currently in use, left as an example
-}
-animal_dict = {
-    'Хочу пёсу!': 'собака',
-    'Хочу коту!': 'котэ'
+    'photo': bot.send_photo  # not currently in use, left as an example
 }
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    unswer = 'Привет, '
-    unswer += message.chat.first_name
-    unswer += '! Хочешь получить пёсика или котика?'
+    unswer = f'Привет, {message.chat.first_name}! Хочешь получить пёсика?'
     print_log('Start', message.chat.first_name)
-    try_send('message', message.chat, unswer, keyboard1)
+    try_send('message', message.chat, unswer, keyboard)
 
 
 @bot.message_handler(content_types=['text'])
 def get_message(message):
     try:
-        command = animal_dict[message.text]
+        command = ANIMAL_DICT[message.text]
         url = get_mp4_url(command)
         print_log(command, message.chat.first_name)
-        try_send('mp4', message.chat, url, keyboard1)
+        try_send('mp4', message.chat, url, keyboard)
     except KeyError:
         start_message(message)
 
